18dbe3184727766f77dfb09f419da25c2b6913d1
victuzor <110581364+victuzor@users.noreply.github.com>
2023-12-18 20:45:43 +0000

    Update Jogo da Forca.cpp

--- Jogo da Forca.cpp
+++ Jogo da Forca.cpp
@@ -10,251 +10,251 @@
 #include <set>
 
 // Classe Base
-class Game {
+class Jogo {
 protected:
-    std::map<std::string, std::vector<std::string>> wordBank;
-    std::string currentWord;
-    std::string displayWord;
-    int maxErrors;
-    int errors;
-    char* usedLetters; // Ponteiro para um array dinâmico
-    int usedLettersCapacity; // Tamanho atual do array
-    std::string theme;
+    std::map<std::string, std::vector<std::string>> banco_palavras;
+    std::string palavra_atual;
+    std::string mostrar_palavra;
+    int maxErros;
+    int erros;
+    char* letrasUsadas; // Ponteiro para um array dinâmico
+    int capacidadeLetrasUsadas; // Tamanho atual do array
+    std::string tema;
 
-    static std::set<std::string> usedWords; // Armazena palavras já utilizadas
+    static std::set<std::string> palavrasUsadas; // Armazena palavras já utilizadas
 
-    void loadWords(std::string filename) {
+    void CarregarPalavras(std::string filename) {
         std::ifstream file(filename);
         if (!file) {
             std::cerr << "Erro ao abrir o arquivo: " << filename << std::endl;
             return;
         }
 
-        std::string line, theme;
-        while (getline(file, line)) {
-            if (line.empty()) continue;
-            if (line.back() == ':') {
-                theme = line.substr(0, line.size() - 1);
-                wordBank[theme] = std::vector<std::string>();
+        std::string linha, tema;
+        while (getline(file, linha)) {
+            if (linha.empty()) continue;
+            if (linha.back() == ':') {
+                tema = linha.substr(0, linha.size() - 1);
+                banco_palavras[tema] = std::vector<std::string>();
             } else {
-                wordBank[theme].push_back(line);
+                banco_palavras[tema].push_back(linha);
             }
         }
 
-        if (wordBank.empty()) {
+        if (banco_palavras.empty()) {
             std::cerr << "Nenhuma palavra carregada do arquivo." << std::endl;
         }
     }
 
-    void chooseWord() {
+    void escolherPalavra() {
         do {
-            int themeIndex = rand() % wordBank.size();
-            auto it = wordBank.begin();
-            std::advance(it, themeIndex);
-            theme = it->first;
-            const std::vector<std::string>& words = it->second;
-            currentWord = words[rand() % words.size()];
-        } while (usedWords.find(currentWord) != usedWords.end()); // Verifica se a palavra já foi usada
+            int indiceTema = rand() % banco_palavras.size();
+            auto iterador = banco_palavras.begin();
+            std::advance(iterador, indiceTema);
+            tema = iterador->first;
+            const std::vector<std::string>& palavras = iterador->second;
+            palavra_atual = palavras[rand() % palavras.size()];
+        } while (palavrasUsadas.find(palavra_atual) != palavrasUsadas.end()); // Verifica se a palavra já foi usada
 
-        usedWords.insert(currentWord); // Adiciona a palavra atual ao conjunto de usadas
+        palavrasUsadas.insert(palavra_atual); // Adiciona a palavra atual ao conjunto de usadas
 
-        displayWord = "";
-        for (char c : currentWord) {
-            displayWord += (c == ' ' ? "  " : "_ ");
+        mostrar_palavra = "";
+        for (char c : palavra_atual) {
+            mostrar_palavra += (c == ' ' ? "  " : "_ ");
         }
-        displayWord.pop_back();
+        mostrar_palavra.pop_back();
     }
 
-    void displayState(int CurrentPlayerErrors) { 
+    void Mostrar(int ErrosdoJogadorAtual) { 
         std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
-        std::cout << "Tema: " << theme << std::endl;
+        std::cout << "Tema: " << tema << std::endl;
         std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
         std::cout << "Palavra: ";
-        for (char c : displayWord) {
+        for (char c : mostrar_palavra) {
             std::cout << c;  // Imprime a palavra com espaços
         }
         std::cout << std::endl;
-        std::cout << "Erros: " << CurrentPlayerErrors << " de " << maxErrors << std::endl;
-        std::cout << "Letras utilizadas: " << usedLetters << std::endl;
+        std::cout << "Erros: " << ErrosdoJogadorAtual << " de " << maxErros << std::endl;
+        std::cout << "Letras utilizadas: " << letrasUsadas << std::endl;
         std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
     }
 
     // Método para verificar se o caractere é uma letra válida de A a Z
-    bool isValidLetter(char letter) {
-        return letter >= 'A' && letter <= 'Z';
+    bool ValidarLetra(char letra) {
+        return letra >= 'A' && letra <= 'Z';
     }
 
 public:
-    Game(std::string filename) : usedLettersCapacity(10) {
-        loadWords(filename);
+    Jogo(std::string filename) : capacidadeLetrasUsadas(10) {
+        CarregarPalavras(filename);
         srand(time(NULL));
-        usedLetters = new char[usedLettersCapacity]; // Alocando memória
-        usedLetters[0] = '\0'; // Inicializar com string vazia
+        letrasUsadas = new char[capacidadeLetrasUsadas]; // Alocando memória
+        letrasUsadas[0] = '\0'; // Inicializar com string vazia
     }
-    virtual void play() = 0;
+    virtual void jogar() = 0;
 
-    virtual ~Game() {
-        delete[] usedLetters; // Liberando memória alocada
+    virtual ~Jogo() {
+        delete[] letrasUsadas; // Liberando memória alocada
     }
 
     
 };
 
-std::set<std::string> Game::usedWords;
+std::set<std::string> Jogo::palavrasUsadas;
 
 
-// Classe SoloGame
-class SoloGame : public Game {
+// Classe JogoSolo
+class JogoSolo : public Jogo {
 public:
-    SoloGame(std::string filename) : Game(filename) {
-        maxErrors = 6;
+    JogoSolo(std::string filename) : Jogo(filename) {
+        maxErros = 6;
     }
 
-    void play() override {
-        int score = 0;
-        bool playAgain = true;
-        while (playAgain) {
-            errors = 0;
-            usedLetters[0] = '\0'; // Reinicializa o array
-            chooseWord();  // Escolhe uma nova palavra e tema
-            while (errors < maxErrors && displayWord.find('_') != std::string::npos) {
-                displayState(errors);
-                char guess;
+    void jogar() override {
+        int pontos = 0;
+        bool Jogardenovo = true;
+        while (Jogardenovo) {
+            erros = 0;
+            letrasUsadas[0] = '\0'; // Reinicializa o array
+            escolherPalavra();  // Escolhe uma nova palavra e tema
+            while (erros < maxErros && mostrar_palavra.find('_') != std::string::npos) {
+                Mostrar(erros);
+                char tentativa;
                 std::cout << "Escolha uma letra: ";
-                std::cin >> guess;
-                guess = toupper(guess);
+                std::cin >> tentativa;
+                tentativa = toupper(tentativa);
 
-                if (!isValidLetter(guess)) {
+                if (!ValidarLetra(tentativa)) {
                 std::cout << "Entrada inválida. Por favor, insira uma letra de A a Z.\n";
                 continue;
                 }
 
-                if (std::string(usedLetters).find(guess) != std::string::npos) {
+                if (std::string(letrasUsadas).find(tentativa) != std::string::npos) {
                     std::cout << "Letra já utilizada. Tente outra.\n";
                     continue;
                 }
 
-                size_t len = strlen(usedLetters);
-                if (len >= usedLettersCapacity - 1) {
-                    usedLettersCapacity *= 2;
-                    char* temp = new char[usedLettersCapacity];
-                    strcpy(temp, usedLetters);
-                    delete[] usedLetters;
-                    usedLetters = temp;
+                size_t tamanho = strlen(letrasUsadas);
+                if (tamanho >= capacidadeLetrasUsadas - 1) {
+                    capacidadeLetrasUsadas *= 2;
+                    char* temp = new char[capacidadeLetrasUsadas];
+                    strcpy(temp, letrasUsadas);
+                    delete[] letrasUsadas;
+                    letrasUsadas = temp;
                 }
 
-                usedLetters[len] = guess;
-                usedLetters[len + 1] = '\0';
+                letrasUsadas[tamanho] = tentativa;
+                letrasUsadas[tamanho + 1] = '\0';
 
-                bool found = false;
-                for (size_t i = 0; i < currentWord.size(); i++) {
-                    if (currentWord[i] == guess) {
-                        displayWord[i * 2] = guess;
-                        found = true;
+                bool letraCerta = false;
+                for (size_t i = 0; i < palavra_atual.size(); i++) {
+                    if (palavra_atual[i] == tentativa) {
+                        mostrar_palavra[i * 2] = tentativa;
+                        letraCerta = true;
                     }
                 }
 
-                if (!found) errors++;
+                if (!letraCerta) erros++;
             }
 
-            displayState(errors);
-            if (displayWord.find('_') == std::string::npos) {
-                std::cout << "Parabéns! Você acertou a palavra: " << currentWord << std::endl;
-                score++;
+            Mostrar(erros);
+            if (mostrar_palavra.find('_') == std::string::npos) {
+                std::cout << "Parabéns! Você acertou a palavra: " << palavra_atual << std::endl;
+                pontos++;
             } else {
-                std::cout << "Você perdeu. A palavra era: " << currentWord << std::endl;
+                std::cout << "Você perdeu. A palavra era: " << palavra_atual << std::endl;
             }
 
             std::cout << "Deseja jogar novamente? (S/N): ";
-            char response;
-            std::cin >> response;
-            playAgain = (toupper(response) == 'S');
+            char resposta;
+            std::cin >> resposta;
+            Jogardenovo = (toupper(resposta) == 'S');
         }
 
-        std::cout << "Total de vitórias nesta sessão: " << score << std::endl;
+        std::cout << "Total de viteradorórias nesta sessão: " << pontos << std::endl;
     }
 };
 
-// Classe MultiplayerGame
-class MultiplayerGame : public Game {
+// Classe Multijogador
+class Multijogador : public Jogo {
 public:
-    MultiplayerGame(std::string filename) : Game(filename) {
-        maxErrors = 6;
+    Multijogador(std::string filename) : Jogo(filename) {
+        maxErros = 6;
     }
 
-    void play() override {
-        std::string players[2];
-        int scores[2] = {0, 0};
-        int errorsPerPlayer[2] = {0, 0};
+    void jogar() override {
+        std::string jogadores[2];
+        int pontos_jogadores[2] = {0, 0};
+        int errosPorJogadores[2] = {0, 0};
         std::cout << "Nome do Jogador 1: ";
-        std::cin >> players[0];
+        std::cin >> jogadores[0];
         std::cout << "Nome do Jogador 2: ";
-        std::cin >> players[1];
+        std::cin >> jogadores[1];
 
-        bool playAgain = true;
-        while (playAgain) {
-            int currentPlayer = 0;
-            errorsPerPlayer[0] = errorsPerPlayer[1] = 0;
-            usedLetters[0] = '\0'; // Reinicializa o array
-            chooseWord();
+        bool Jogardenovo = true;
+        while (Jogardenovo) {
+            int JogadorAtual = 0;
+            errosPorJogadores[0] = errosPorJogadores[1] = 0;
+            letrasUsadas[0] = '\0'; // Reinicializa o array
+            escolherPalavra();
 
-            while (errorsPerPlayer[0] < maxErrors && errorsPerPlayer[1] < maxErrors &&
-                   displayWord.find('_') != std::string::npos) {
-                displayState(errorsPerPlayer[currentPlayer]);
-                std::cout << "Turno de " << players[currentPlayer] << std::endl;
-                char guess;
+            while (errosPorJogadores[0] < maxErros && errosPorJogadores[1] < maxErros &&
+                   mostrar_palavra.find('_') != std::string::npos) {
+                Mostrar(errosPorJogadores[JogadorAtual]);
+                std::cout << "Turno de " << jogadores[JogadorAtual] << std::endl;
+                char tentativa;
                 std::cout << "Escolha uma letra: ";
-                std::cin >> guess;
-                guess = toupper(guess);
+                std::cin >> tentativa;
+                tentativa = toupper(tentativa);
 
-                if (!isValidLetter(guess)) {
+                if (!ValidarLetra(tentativa)) {
                 std::cout << "Entrada inválida. Por favor, insira uma letra de A a Z.\n";
                 continue;
                 }
 
-                if (std::string(usedLetters).find(guess) == std::string::npos) {
-                    size_t len = strlen(usedLetters);
-                    if (len >= usedLettersCapacity - 1) {
-                        usedLettersCapacity *= 2;
-                        char* temp = new char[usedLettersCapacity];
-                        strcpy(temp, usedLetters);
-                        delete[] usedLetters;
-                        usedLetters = temp;
+                if (std::string(letrasUsadas).find(tentativa) == std::string::npos) {
+                    size_t tamanho = strlen(letrasUsadas);
+                    if (tamanho >= capacidadeLetrasUsadas - 1) {
+                        capacidadeLetrasUsadas *= 2;
+                        char* temp = new char[capacidadeLetrasUsadas];
+                        strcpy(temp, letrasUsadas);
+                        delete[] letrasUsadas;
+                        letrasUsadas = temp;
                     }
 
-                    usedLetters[len] = guess;
-                    usedLetters[len + 1] = '\0';
+                    letrasUsadas[tamanho] = tentativa;
+                    letrasUsadas[tamanho + 1] = '\0';
                 }
 
-                bool found = false;
-                for (size_t i = 0; i < currentWord.size(); i++) {
-                    if (currentWord[i] == guess) {
-                        displayWord[i * 2] = guess;
-                        found = true;
+                bool letraCerta = false;
+                for (size_t i = 0; i < palavra_atual.size(); i++) {
+                    if (palavra_atual[i] == tentativa) {
+                        mostrar_palavra[i * 2] = tentativa;
+                        letraCerta = true;
                     }
                 }
 
-                if (!found) {
-                    errorsPerPlayer[currentPlayer]++;
+                if (!letraCerta) {
+                    errosPorJogadores[JogadorAtual]++;
                 }
 
-                currentPlayer = (currentPlayer + 1) % 2;
+                JogadorAtual = (JogadorAtual + 1) % 2;
             }
 
-            displayState(errorsPerPlayer[currentPlayer]);
-            if (displayWord.find('_') == std::string::npos) {
-                int winningPlayer = (currentPlayer + 1) % 2;
-                std::cout << "Parabéns, " << players[winningPlayer] << "! Você acertou a palavra: " << currentWord << std::endl;
-                scores[winningPlayer]++;
+            Mostrar(errosPorJogadores[JogadorAtual]);
+            if (mostrar_palavra.find('_') == std::string::npos) {
+                int winningjogarer = (JogadorAtual + 1) % 2;
+                std::cout << "Parabéns, " << jogadores[winningjogarer] << "! Você acertou a palavra: " << palavra_atual << std::endl;
+                pontos_jogadores[winningjogarer]++;
             } else {
-                std::cout << "Nenhum jogador acertou. A palavra era: " << currentWord << std::endl;
+                std::cout << "Nenhum jogador acertou. A palavra era: " << palavra_atual << std::endl;
             }
 
-            std::cout << "Placar: " << players[0] << " " << scores[0] << ", " << players[1] << " " << scores[1] << std::endl;
+            std::cout << "Placar: " << jogadores[0] << " " << pontos_jogadores[0] << ", " << jogadores[1] << " " << pontos_jogadores[1] << std::endl;
             std::cout << "Deseja jogar novamente? (S/N): ";
-            char response;
-            std::cin >> response;
-            playAgain = (toupper(response) == 'S');
+            char resposta;
+            std::cin >> resposta;
+            Jogardenovo = (toupper(resposta) == 'S');
         }
     }
 };
@@ -263,23 +263,23 @@ int main() {
     std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
     std::cout << "            Jogo da Forca" << std::endl;
     std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
-    std::cout << "Digite 1 para jogar o modo solo." << std::endl;
-    std::cout << "Digite 2 para jogar o modo multijogador." << std::endl;
+    std::cout << "Digiteradore 1 para jogar o modo solo." << std::endl;
+    std::cout << "Digiteradore 2 para jogar o modo multijogador." << std::endl;
     std::cout << "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" << std::endl;
-    std::cout << "Digite aqui: ";
+    std::cout << "Digiteradore aqui: ";
 
-    int choice;
-    std::cin >> choice;
+    int escolha;
+    std::cin >> escolha;
 
-    switch(choice) {
+    switch(escolha) {
         case 1: {
-            SoloGame soloGame("wordlist.txt");
-            soloGame.play();
+            JogoSolo JogoSolo("wordlist.txt");
+            JogoSolo.jogar();
             break;
         }
         case 2: {
-            MultiplayerGame multiplayerGame("wordlist.txt");
-            multiplayerGame.play();
+            Multijogador Multijogador("wordlist.txt");
+            Multijogador.jogar();
             break;
         }
         default:
